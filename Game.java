69f86dc794210421f31b17dbd66eff6a1e979aee
Rikachy <richardlife101@gmail.com>
2025-11-01 16:30:14 -0400

    up to 18

--- Game.java
+++ Game.java
@@ -145,8 +145,9 @@ public class Game
         System.out.println("You are lost. You are alone. You wander");
         System.out.println("around at the university.");
         System.out.println();
-        System.out.println("Your comman d words are:");
-        System.out.println("   go quit help look drink");
+        System.out.println("Your command words are:");
+        //System.out.println("   go quit help look drink");
+        //no longer needed 
         parser.helpCommands();
     }
 
